1169fbf8e0b26dbf86d65dcf6707780c67eca693
simontijanic <simontijanicerkul@gmail.com>
2024-10-21 11:23:41 +0200

    front end

--- public/client.js
+++ public/client.js
@@ -11,32 +11,35 @@ socket.on('chat message', (data) => {
     li.className = 'other-message'; // Class for others' messages
   }
 
-    li.textContent = `${data.user}: ${data.message}`;
+  li.innerHTML = `<strong>${data.user}:</strong> ${data.message}`; // Use innerHTML to render HTML    
   messages.appendChild(li);
 });
 
 document.getElementById('messageForm').addEventListener('submit', (e) => {
   e.preventDefault();
   const input = document.getElementById('messageInput');
+  const errorMessage = document.getElementById(`errorMessage`)
   const message = input.value.trim();
 
   if (message === '') {
     console.error('Message cannot be empty!'); 
-    input.placeholder = 'Message cannot be empty!';
+    errorMessage.innerHTML = 'Message cannot be empty!';
     return;
   }
   
   if (message.length > 200) {
     console.error('Message cannot exceed 200 characters!');
-    input.placeholder = 'Message cannot exceed 200 characters!'; 
+    errorMessage.innerHTML = 'Message cannot exceed 200 characters!'; 
     return;
   }
 
   input.placeholder = '';
   socket.emit('chat message', { user: username, message });
   input.value = ''; 
+  errorMessage.innerHTML = '';
 
   input.addEventListener('input', () => {
     input.placeholder = ''; // Clear the placeholder when user starts typing
+    errorMessage.innerHTML = '';
   });  
 });
